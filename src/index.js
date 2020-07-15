ef8acf9d232ab831d55edbee89fa2749fe735611
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2020-07-15 11:27:30 +0545

    Check before rendering to a dom

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import Calculator from './Components/Calculator/index';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Calculator/>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('wp-calculator')
-);
+const wpc_element = document.getElementById( 'wp-calculator-container' );
+
+if ( wpc_element ) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Calculator/>
+      </Provider>
+    </React.StrictMode>,
+    wpc_element
+  );
+}
